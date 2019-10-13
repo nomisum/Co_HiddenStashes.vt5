a7a3cb5c9ebcf8a2eebe7a409e5b9278fe7e3b91
nomisum <nomisum@googlemail.com>
2019-10-13 20:19:35 +0200

    remove offroad fnc

--- custom/offroadLight/XEH_InitPost_eh.hpp
+++
@@ -1,3 +0,0 @@
-class I_E_Offroad_01_comms_F {
-    init = "[(_this select 0)] remoteExec wita_offroadLight_fnc_addAction;";
-};
--- custom/offroadLight/cfgFunctions.hpp
+++
@@ -1,8 +0,0 @@
-class wita_offroadLight {
-    tag="wita_offroadLight";
-    class offroadLight {
-        file = "custom\offroadLight";
-        class addAction;
-        class orangeLight;
-    };
-};
