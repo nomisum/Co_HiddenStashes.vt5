7f3cfefa1e75ff5f482f555f74acf64fe0c3ead5
simon.schmidt <simon.schmidt@idealo.de>
2019-10-27 14:27:51 +0100

    exchange apc for heli as vehicle

--- buyables/blu_ldf/vehicles.hpp
+++ buyables/blu_ldf/vehicles.hpp
@@ -11,19 +11,19 @@ class Vehicles {
         permissionLevel = 0;
     };
 
-    class I_E_APC_tracked_03_cannon_F {
-        displayName = "APC FV720";
-        description = "Seats: 10<br/><br/>";
-        price = 2000;
-        stock = 1;
-        permissionLevel = 1;
-    };
-
     class I_E_Heli_light_03_unarmed_F {
         displayName = "Wildcat";
         description = "Seats: 8<br/><br/>";
         price = 2000;
         stock = 3;
-        permissionLevel = 2;
+        permissionLevel = 1;
     };
+
+    class I_E_APC_tracked_03_cannon_F {
+        displayName = "APC FV720";
+        description = "Seats: 10<br/><br/>";
+        price = 3000;
+        stock = 1;
+        permissionLevel = 2;
+    };    
 };
