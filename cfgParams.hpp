65ff2d855821d6cc2d62d34526c44c3f34034fcd
nomisum <nomisum@googlemail.com>
2019-11-03 18:57:31 +0100

    wave size 2

--- cfgParams.hpp
+++ cfgParams.hpp
@@ -32,7 +32,7 @@ class Params {
         title = "BLUFOR Respawn Wave Size";
         values[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
         texts[] = {"Automatic", "1", "2","3","4","5","6","7","8","9","10","11","12","13","14","15"};
-        default = 3;
+        default = 2;
     };
 
     class RESPAWNTIME
